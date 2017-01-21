61bc6265c505120d2263431faeebfc0b41e66ea2
JLRC <tangun73@gmail.com>
2017-01-21 18:24:26 +0100

    changed email

--- js/main.js
+++ js/main.js
@@ -31,6 +31,23 @@ $(document).ready(function () {
     $('.navbar-toggle').on('click', function (e) {
         $(this).toggleClass('open');        
     });
+    
+    $(".eh__btn").on('click', function(e){
+       var lang = $(this).attr("lang");
+        $.post("https://formspree.io/estefaniayhugo2017@gmail.com", $("#confirm_form").serialize(), function(){
+           switch(lang){
+               case "en":
+                   alert("some english stuff");
+                   break;
+               case "es":
+                   alert("some spanish stuff");
+                   break;
+               case "pt":
+                alert("some portuguese stuff");
+                   break;
+           }
+       }) 
+    });
 
 
 });
